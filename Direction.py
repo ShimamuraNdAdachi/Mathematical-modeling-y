d111cf9a60e1015ba635b8a77aebee84bb2abd5a
chmc123 <ychmc123@163.com>
2025-05-27 22:23:15 +0800

    框架大部分修改： 1、将Warehouse成员变量robot_positions修改为缓存，并增加了关于此变量的刷新方法 2、新增tick方法，用于自动通过路径列表自动移动，并返回消耗时间和总成功移动次数 3、Direction增加将整形坐标转化为Direction枚举常量的方法

--- Direction.py
+++ Direction.py
@@ -5,3 +5,13 @@ class Direction(Enum):
     DOWN = (0, 1)  # 向下移动时y增加
     LEFT = (-1, 0)  # 向左移动时x减小
     RIGHT = (1, 0)  # 向右移动时x增加
+
+    COORDINATES_TO_DIRECTION = {
+        (0, -1): UP,
+        (0, 1): DOWN,
+        (-1, 0): LEFT,
+        (1, 0): RIGHT,
+    }
+    @staticmethod
+    def coordinates_to_direction(dx, dy):
+        return Direction.COORDINATES_TO_DIRECTION.get((dx, dy))
